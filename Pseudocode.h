1f6914377a09eeb1307f6d8ca9b6b78379f651bb
mb6ff <mariebiernacki.art@gmail.com>
2024-10-27 16:05:43 -0500

    Add project files.

---
+++ Pseudocode.h
@@ -0,0 +1,28 @@
+#pragma once
+/*
+PART 1
+create Queue (using linked list or circular array) -- SHOULD BE TEMPLATE CLASS
+	push()
+	pop()
+	front()
+	size()
+	empty()
+
+instantiate the queue with integers and push 10 values onto the queue
+display all the elements in a queue only using the above functions
+
+write new function called 
+	move_to_rear
+		takes the element at the front of the queue to the rear of the queue
+		use functions push, front, pop
+
+PART 2
+write recursive linear search function
+finds the last occurrence of a target in a vector, NOT the first
+modify the code from Lecture 9, Slide 30
+
+PART 3
+modify insertion sort to sort a list of integers (either queue or linked list implementation)
+source code on lecture 10 slide 38
+
+*/
